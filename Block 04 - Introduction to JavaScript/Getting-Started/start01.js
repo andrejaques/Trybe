2eb9b6a28395a72ca31ebebf6bb90365c56e9b5d
André Jaques Batista <andrejaques.b@gmail.com>
2021-02-24 15:21:39 -0300

    começando no javascript

---
+++ Block 04 - Introduction to JavaScript/Getting-Started/start01.js
@@ -0,0 +1,19 @@
+const name = "André Jaques";
+const birthCity = "Araripina";
+let birthYear = 1991
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+
+console.log() // \n 
+
+birthYear = 2020;
+console.log(birthYear);
+
+console.log() // \n
+
+/* birthCity = "Xablau"
+console.log(birthCity)
+    // saída erro por conta de alteração de uma constante. 
+*/
---
+++ Block 04 - Introduction to JavaScript/Getting-Started/start02.js
@@ -0,0 +1,17 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+    firstName: 'Ana',
+    lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log() // \n
+
+let base = 5;
+let altura = 8;
+let area = base * altura;
+let perimetro = (base * 2) + (altura * 2);
+
+console.log(area);
+console.log(perimetro);
