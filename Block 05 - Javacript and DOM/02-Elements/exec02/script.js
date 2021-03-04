2d020954567e88b01b4e5393bef29aa9437f0daa
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-04 18:40:08 -0300

    add the require 01

---
+++ Block 05 - Javacript and DOM/02-Elements/exec02/script.js
@@ -0,0 +1,16 @@
+// scripando
+
+// require 01
+let body = document.querySelector('body');
+
+let h1 = document.createElement('h1');
+h1.id = 'title';
+h1.innerHTML = "Exercício 5.2 - JavaScript DOM";
+h1.style.background = "rgb(117, 117, 117)";
+h1.style.color = "green";
+h1.style.textAlign = "center";
+h1.style.textShadow = "3px 3px 1px black"
+
+body.appendChild(h1);
+
+// require 02
