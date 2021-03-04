bf581c74ce60c2574bc4d4cd54332f97b9551fcd
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-04 15:42:54 -0300

    add requires 4, 5 and 6

--- Block 05 - Javacript and DOM/02-Elements/exec01/script.js
+++ Block 05 - Javacript and DOM/02-Elements/exec01/script.js
@@ -1,8 +1,20 @@
 // require 01
 let elementoOndeVoceEsta = document.querySelector("#elementoOndeVoceEsta");
+
 // require 02
 let pai = elementoOndeVoceEsta.parentNode;
 pai.style.backgroundColor = "#ddd";
+
 // require 03
 let primeiroFilhoDoFilho = document.querySelector("#primeiroFilhoDoFilho");
 primeiroFilhoDoFilho.innerHTML = "Sou o primeiro filho do filho";
+
+// require 04
+let primeiroFilho = pai.firstChild;
+
+// require 05
+primeiroFilho = elementoOndeVoceEsta.previousElementSibling;
+
+// require 06
+let atencao = elementoOndeVoceEsta.nextSibling;
+console.log(atencao)
