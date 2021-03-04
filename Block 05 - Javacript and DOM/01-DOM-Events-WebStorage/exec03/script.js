2b0e1b977c924a2896b09dc8ae7b1035fb6bdc6e
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-04 10:33:36 -0300

    add request 04

--- Block 05 - Javacript and DOM/01-DOM-Events-WebStorage/exec03/script.js
+++ Block 05 - Javacript and DOM/01-DOM-Events-WebStorage/exec03/script.js
@@ -9,6 +9,11 @@ function changeColor (element, color) {
     element01.style.backgroundColor = color;
 }
 
+// request 01
 changeText("p", "Fazendo entrevista para uma vaga pleno", 1);
+//request 02
 changeColor(".main-content", "rgb(76,164,109)");
+// request 03
 changeColor(".center-content", "white");
+// request 04
+changeText("h1", "Exercício 5.1 - JavaScript (EC6)", 0);
