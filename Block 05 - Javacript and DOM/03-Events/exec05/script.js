a6f5e0ada7a6c4d21809ee6b1765a3bf3cf8b0bd
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-05 19:11:58 -0300

    add first question

---
+++ Block 05 - Javacript and DOM/03-Events/exec05/script.js
@@ -0,0 +1,28 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+
+    for (let index = 0; index < weekDays.length; index += 1) {
+        const days = weekDays[index];
+        const dayListItem = document.createElement('li');
+        dayListItem.innerHTML = days;
+
+        weekDaysList.appendChild(dayListItem);
+    };
+};
+
+function createDays() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const weekDaysList = document.querySelector('#days');
+
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        const days = dezDaysList[index];
+        const dayListItem = document.createElement('li');
+        dayListItem.innerHTML = days;
+        
+        weekDaysList.appendChild(dayListItem);
+    };
+};
+
+createDaysOfTheWeek();
+createDays();
